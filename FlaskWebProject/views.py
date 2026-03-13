21f1f8d4a866cec1ec8c82d2a0bfe79fd596cf18
Prince Raj <114799883+theprince09@users.noreply.github.com>
2026-03-13 20:31:00 +0530

    Update warning message for invalid login attempts

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -66,7 +66,7 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
-            app.logger.warning("Invalid login attempt")
+            app.logger.warning("Invalid login attempt.....")
             flash('Invalid username or password')
             return redirect(url_for('login'))
         else:
